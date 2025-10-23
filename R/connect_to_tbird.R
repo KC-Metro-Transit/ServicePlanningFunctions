b578e98ef27e9631e69403aad457c3862d4551a9
Melissa Gaughan <gaughan.melissa@gmail.com>
2025-10-23 15:55:02 -0700

    connect to t-bird function

---
+++ R/connect_to_tbird.R
@@ -0,0 +1,19 @@
+connect_to_tbird <- function() {
+  con <- DBI::dbConnect(
+    odbc::odbc(),
+    Driver = "ODBC Driver 17 for SQL Server",
+    Server = "kcitazrsqlprp01.database.windows.net",
+    Database = "tbird_dw",
+    Authentication = "ActiveDirectoryIntegrated"
+  )
+
+  if (DBI::dbIsValid(con) == TRUE) {
+    cli::cli_alert_success(
+      text = "Connection to T-BIRD successful"
+    )
+  } else {
+    cli::cli_alert_warning(
+      text = "Connection to T-BIRD NOT successful. Are you on the VPN?"
+    )
+  }
+}
