8e98b11fe24f51ca5fe41eb461d8335745bffc64
Melissa Gaughan <gaughan.melissa@gmail.com>
2026-03-04 15:04:16 -0800

    change colnames to lowercase add ref and defs to trip crosstab plot

--- R/plot_trip_crosstab.R
+++ R/plot_trip_crosstab.R
@@ -8,10 +8,10 @@
 #' @param x_axis Character. Grouping variable based on columns found in output from get_trip_ridership(). Day, period, hour.
 #' @param activity_type Character. ons - Average Daily Boarding, offs - Average Daily Alightings, avg_lod - Average Max Load.
 #'
-#' @returns
+#' @returns ggplot2 plot of ons, offs, and load by Select Variable and Service Change from get_trip_ridership()
 #'
 #' @export
-#' @examples
+
 plot_trip_crosstab <- function(
   dataframe,
   service_change_num,
@@ -21,24 +21,19 @@ plot_trip_crosstab <- function(
   x_axis,
   activity_type = 'ons'
 ) {
-  #if (x_axis == 'neighborhood') {
-  #  data1 <- trip_ridership %>%
-  #    left_join(xwalk_route_neighborhood, by = join_by(route))
-  #}
-
   data <- dataframe %>%
     dplyr::filter(
-      Day %in% day,
+      day %in% day,
       day_part_cd %in% time_period,
       service_change_num %in% .env$service_change_num,
       route %in% .env$route
     )
 
   plot_data <- data %>%
-    dplyr::group_by_at(vars(service_change_num, Service, x_axis)) %>%
+    dplyr::group_by_at(dplyr::vars(service_change_num, service, x_axis)) %>%
     dplyr::select(
       service_change_num,
-      Service,
+      service,
       'axis' = x_axis,
       ons:avg_load
     ) %>%
@@ -76,7 +71,7 @@ plot_trip_crosstab <- function(
     'hour' ~ 'Hour',
     'route' ~ 'Route',
     'stop' ~ 'Stop',
-    .default = str_to_title(x_axis)
+    .default = stringr::str_to_title(x_axis)
   )
 
   day_title <- ifelse(
@@ -103,36 +98,40 @@ plot_trip_crosstab <- function(
   plt <- ggplot2::ggplot(
     plot_data,
     aes(
-      x = reorder(axis, desc(value)),
+      x = stats::reorder(axis, dplyr::desc(value)),
       y = value,
-      fill = reorder(Service, service_change_num)
+      fill = stats::reorder(service, service_change_num)
     )
   )
 
   if (x_axis == 'period') {
     plt <- ggplot2::ggplot(
       plot_data,
-      aes(x = axis, y = value, fill = reorder(Service, service_change_num))
+      aes(
+        x = axis,
+        y = value,
+        fill = stats::reorder(service, service_change_num)
+      )
     )
   }
   if (x_axis == 'hour') {
     plot_data <- plot_data %>%
-      mutate(hour_label = as.character(axis))
+      dplyr::mutate(hour_label = as.character(axis))
 
-    plt <- ggplot(
+    plt <- ggplot2::ggplot(
       plot_data,
       aes(
-        x = reorder(hour_label, axis),
+        x = stats::reorder(hour_label, axis),
         y = value,
-        fill = reorder(Service, service_change_num)
+        fill = stats::reorder(service, service_change_num)
       )
     )
   }
 
   plt <- plt +
-    geom_col(position = position_dodge()) +
+    ggplot2::geom_col(position = ggplot2::position_dodge()) +
     viridis::scale_fill_viridis(discrete = TRUE, name = 'Legend') +
-    ggtitle(paste0(
+    ggplot2::ggtitle(paste0(
       var_title,
       ' by ',
       axis_title,
@@ -140,10 +139,10 @@ plot_trip_crosstab <- function(
       'Trip Ridership'
     )) +
     ggplot2::labs(subtitle = paste(day_title, period_title, sep = ", ")) +
-    scale_x_discrete(
+    ggplot2::scale_x_discrete(
       labels = scales::label_wrap(10),
       guide = guide_axis(angle = 45)
     ) +
-    style_kcm()
+    ServicePlanningFunctions::style_kcm()
   plt
 }
