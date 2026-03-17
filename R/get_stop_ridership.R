a565a1c57f3c2fa13b93722462363e48cf3eda0c
Melissa Gaughan <gaughan.melissa@gmail.com>
2026-03-17 16:22:11 -0700

    correct logic error caused by "All"

--- R/get_stop_ridership.R
+++ R/get_stop_ridership.R
@@ -2,7 +2,7 @@
 #' Export stop level ons, offs and load from DP.STOP_ACTIVITY_DETAIL
 #' @param service_change_num Numeric. The three-digit identifier of the service change.
 #'  Can accept multiple values as a vector.
-#' @param route Numeric. The route identifiers of interest. Values to be treated as characters to allow for non-numeric route identifiers.
+#' @param route Numeric. The route identifiers of interest or "All" to return all routes. Values to be treated as characters to allow for non-numeric route identifiers.
 #' Can accept multiple values as a vector.
 #' @param stop_id A vector of stop IDs or "All" if all stops on a route are of interest.
 #' @param tbird_connection The connection object created by connect_to_tbird()
@@ -17,12 +17,53 @@ get_stop_ridership <- function(
   stop_id,
   tbird_connection
 ) {
-  if (stop_id == "All") {
+  return_all_stops <- if ("All" %in% stop_id) {
+    TRUE
+  } else {
+    FALSE
+  }
+
+  return_all_routes <- if ("All" %in% route) {
+    TRUE
+  } else {
+    FALSE
+  }
+
+  if (return_all_stops == TRUE & return_all_routes == TRUE) {
+    cli::cli_alert(text = "Returning system.")
     stop_data <- DBI::dbGetQuery(
       tbird_connection,
       glue::glue_sql(
-        "
-  SELECT [SERVICE_CHANGE_NUM]
+        " SELECT [SERVICE_CHANGE_NUM]
+      ,[SERVICE_RTE_NUM] as route
+      ,[TRIP_ID]
+      ,[INBD_OUTBD_CD] as direction
+      ,[STOP_ID]
+      ,[BEARING_CD]
+      ,[HOST_STREET_NM]
+      ,[CROSS_STREET_NM]
+      ,[JURISDICTION_CD]
+      ,[STOP_SEQUENCE_NUM]
+      ,[AVG_ARRIVAL_CLOCK_TIME]
+      ,[AVG_ARRIVAL_MNTS_AFTER_MIDNT] as arrive
+      ,[DAY_PART_CD]
+      ,[AVG_PSNGR_BOARDINGS] as ons
+      ,[AVG_PSNGR_ALIGHTINGS] as offs
+      ,[AVG_LOAD] as avg_load
+      ,[OBSERVED_TRIPS]
+  FROM [DP].[STOP_ACTIVITY_DETAIL]
+  WHERE [SERVICE_CHANGE_NUM] IN ({vals1*})
+  ",
+        vals1 = service_change_num,
+        .con = tbird_connection
+      )
+    )
+  } else if (return_all_stops == TRUE & return_all_routes == FALSE) {
+    cli::cli_alert(text = "Returning all stops on selected routes")
+    stop_data <- DBI::dbGetQuery(
+      tbird_connection,
+      glue::glue_sql(
+        "SELECT [SERVICE_CHANGE_NUM]
       ,[SERVICE_RTE_NUM] as route
       ,[TRIP_ID]
       ,[INBD_OUTBD_CD] as direction
@@ -48,12 +89,45 @@ AND SERVICE_RTE_NUM IN ({vals2*})
         .con = tbird_connection
       )
     )
-  } else {
+  } else if (return_all_stops == FALSE & return_all_routes == TRUE) {
+    cli::cli_alert(text = "Returning data for selected stops.")
     stop_data <- DBI::dbGetQuery(
       tbird_connection,
       glue::glue_sql(
-        "
-  SELECT [SERVICE_CHANGE_NUM]
+        "SELECT [SERVICE_CHANGE_NUM]
+      ,[SERVICE_RTE_NUM] as route
+      ,[TRIP_ID]
+      ,[INBD_OUTBD_CD] as direction
+      ,[STOP_ID]
+      ,[BEARING_CD]
+      ,[HOST_STREET_NM]
+      ,[CROSS_STREET_NM]
+      ,[JURISDICTION_CD]
+      ,[STOP_SEQUENCE_NUM]
+      ,[AVG_ARRIVAL_CLOCK_TIME]
+      ,[AVG_ARRIVAL_MNTS_AFTER_MIDNT] as arrive
+      ,[DAY_PART_CD]
+      ,[AVG_PSNGR_BOARDINGS] as ons
+      ,[AVG_PSNGR_ALIGHTINGS] as offs
+      ,[AVG_LOAD] as avg_load
+      ,[OBSERVED_TRIPS]
+  FROM [DP].[STOP_ACTIVITY_DETAIL]
+  WHERE [SERVICE_CHANGE_NUM] IN ({vals1*})
+AND stop_id IN ({vals2*})
+  ",
+        vals1 = service_change_num,
+        vals2 = stop_id,
+        .con = tbird_connection
+      )
+    )
+  } else {
+    cli::cli_alert(
+      text = "Returning data filtered by route and stop."
+    )
+    stop_data <- DBI::dbGetQuery(
+      tbird_connection,
+      glue::glue_sql(
+        " SELECT [SERVICE_CHANGE_NUM]
       ,[SERVICE_RTE_NUM] as route
       ,[TRIP_ID]
       ,[INBD_OUTBD_CD] as direction
@@ -115,10 +189,10 @@ AND SERVICE_RTE_NUM IN ({vals2*})
       ),
       # Convert Year Digits to YYYY
       year = dplyr::case_when(
-        yr > 90 ~ str_c("19", yr), # 1991-1999
+        yr > 90 ~ stringr::str_c("19", yr), # 1991-1999
         yr == "" ~ "2000", # 2000
-        yr %in% 1:10 ~ str_c("200", yr), # 2001-2009
-        yr >= 10 ~ str_c("20", yr), # > 2010
+        yr %in% 1:10 ~ stringr::str_c("200", yr), # 2001-2009
+        yr >= 10 ~ stringr::str_c("20", yr), # > 2010
         TRUE ~ NA
       ),
       day = factor(day, levels = c("Weekday", "Saturday", "Sunday")),
