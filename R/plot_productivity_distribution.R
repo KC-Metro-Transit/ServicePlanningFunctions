d5973abe96197bd1b96cacee9812b35c1109dc84
Michael Shin <122759237+statshin@users.noreply.github.com>
2026-05-21 15:49:57 -0700

    20260521|MS|Add route_gain, route_maintain, and route_loss parameters to plot_productivity_distribution

--- R/plot_productivity_distribution.R
+++ R/plot_productivity_distribution.R
@@ -4,9 +4,12 @@
 #'
 #' @param service_change Numeric. The three-digit identifier of the service change.
 #' @param tbird_connection The connection object created by connect_to_tbird()
-#' @param route Numeric. The route identifiers of interest to highlight and label. Values to be treated as characters to allow for non-numeric route identifiers. Can accept multiple values as a vector. Defaults to show all routes.
+#' @param route Numeric. The route identifiers of interest to highlight and label. Values to be treated as characters to allow for non-numeric route identifiers. Can accept multiple values as a vector. Defaults to show all routes. If any of the route_gain, route_maintain, or route_lose paramenters are populated, this parameter is ignored.
+#' @param route_gain Numeric. Routes to color as gaining service. Overrides route parameter if used.
+#' @param route_maintain Numeric. Routes to color as maintaining service. Overrides route parameter if used.
+#' @param route_lose Numeric. Routes to color as losing service. Overrides route parameter if used.
 #' @param activity_type Character. rides_per_platform_hour - Rides per Platform Hour, psngr_miles_per_platform_mile - Passenger Miles per Platform Mile
-#' @param binwidth Numeric. Specifies bin width. Decrease if dots are too close to each other vertically. Increase if dots are too close to each other horizontally.
+#' @param binwidth Numeric. Specifies bin width. Decrease if dots are too close to each other vertically. Increase if dots are too close to each other horizontally. Defaults to 2 for rides per platform hour and 1 for passenger miles per platform miles.
 #' @param point_size Numeric. The diameter of the dots. Adjust so that dots do not overlap one another. Interacts with binwidth.
 #' @param label_size Numeric. The font size of the dot labels. Adjust so that labels fit within each dot.
 #' @param style_size Numeric. Specifies the sizing style to use with style_kcm(). Defaults to default size. "large" to increase sizing style for pdf/png/jpeg/markdown exports.
@@ -20,10 +23,14 @@
 #' # Generate Rides per Platform Hour distribution dotplot for Fall 2025 Service Change
 #' plot_productivity_distribution(service_change = 253, tbird_connection = con)
 #'
+#' # Highlight Routes based on Service Gain/Loss
+#' plot_productivity_distribution(service_change = 253, tbird_connection = con, route_gain = c(8), route_maintain = c(1, 2), route_lose = c(107, 673))
+#'
 #' # Generate Passenger Miles per Platform Mile distribution dotplot for Fall 2025 Service Change highlighting RapidRide routes only.
 #' # Sizing parameters formatted for png download size.
 #' plot_productivity_distribution(service_change = 253,
-#'  tbird_connection = con, route = c(671, 672, 673, 674, 675, 676, 677, 678),
+#'  tbird_connection = con,
+#'  route = c(671, 672, 673, 674, 675, 676, 677, 678),
 #'  activity_type = 'psngr_miles_per_platform_mile',
 #'  binwidth = 1,
 #'  point_size = 6.5,
@@ -35,8 +42,11 @@ plot_productivity_distribution <- function(
   service_change,
   tbird_connection,
   route = NULL,
+  route_gain = NULL,
+  route_maintain = NULL,
+  route_lose = NULL,
   activity_type = 'rides_per_platform_hour',
-  binwidth = 2,
+  binwidth = NULL,
   point_size = 20,
   label_size = 4,
   style_size = NULL
@@ -60,7 +70,17 @@ plot_productivity_distribution <- function(
       sep = "",
       remove = FALSE
     ) |>
+    # Exclude routes that are missing ridership data (DART Routes)
     dplyr::filter(ons > 0)
+
+  # Set default binwidth values based on activity_type if not set
+  if (is.null(binwidth)) {
+    if (activity_type == 'rides_per_platform_hour') {
+      binwidth <- 2
+    } else if (activity_type == 'psngr_miles_per_platform_mile') {
+      binwidth <- 1
+    }
+  }
 
   binwidth_seq <- seq(0, 100, binwidth)
 
@@ -68,11 +88,25 @@ plot_productivity_distribution <- function(
     dplyr::mutate(
       group = factor(
         ifelse(
-          service_rte_num %in% .env$route | is.null(.env$route),
+          is.null(c(route_gain, route_maintain, route_lose, .env$route)),
           'Route',
-          'System'
+          dplyr::case_when(
+            service_rte_num %in% route_gain ~ 'Gains Service',
+            service_rte_num %in% route_maintain ~ 'Maintains Service',
+            service_rte_num %in% route_lose ~ 'Loses Service',
+            service_rte_num %in%
+              .env$route &
+              is.null(c(route_gain, route_maintain, route_lose)) ~ 'Route',
+            .default = 'System'
+          )
         ),
-        levels = c('Route', 'System')
+        levels = c(
+          'Route',
+          'Gains Service',
+          'Maintains Service',
+          'Loses Service',
+          'System'
+        )
       ),
       binwidth = sapply(value, function(x) {
         binwidth_seq[which.min(ifelse(
@@ -86,10 +120,13 @@ plot_productivity_distribution <- function(
       })
     )
 
+  # Calculate percentiles based on whole network
   percentiles <- quantile(data$value, probs = c(0.25, 0.5, 0.75))
 
   selection <- data.frame(percentile = names(percentiles), value = percentiles)
 
+  # Generate dotplot and save x-y coordinates
+  # See https://stackoverflow.com/questions/44991607/how-do-i-label-the-dots-of-a-geom-dotplot-in-ggplot2
   dotplot <- ggplot2::ggplot(data, ggplot2::aes(x = value, fill = group)) +
     ggplot2::geom_dotplot(
       method = 'histodot',
@@ -110,19 +147,22 @@ plot_productivity_distribution <- function(
   color_legend <- c(
     'Route' = '#0072BC',
     'System' = '#EFEFEF',
+    'Gains Service' = '#006633',
+    'Maintains Service' = '#0072BC',
+    'Loses Service' = '#F57F29',
     "L" = "white",
     "E" = "#FDB71A"
   )
 
-  plt <- ggplot2::ggplot(data2, ggplot2::aes(x = value, fill = group)) +
+  # Plot points and labels using x-y coordinates from dotplot generated above
+  plt <- ggplot2::ggplot(data2, ggplot2::aes(x = value)) +
     ggplot2::geom_point(
       ggplot2::aes(
         x = xtext,
         y = ytext,
         color = group
       ),
-      size = point_size,
-      show.legend = FALSE
+      size = point_size
     ) +
     ggplot2::geom_text(
       ggplot2::aes(
@@ -158,8 +198,20 @@ plot_productivity_distribution <- function(
     ) +
     ggplot2::scale_color_manual(
       values = color_legend,
-      breaks = c('L', 'E'),
-      labels = c('Local', 'Express')
+      breaks = c(
+        'Gains Service',
+        'Maintains Service',
+        'Loses Service',
+        'L',
+        'E'
+      ),
+      labels = c(
+        'Gains Service',
+        'Maintains Service',
+        'Loses Service',
+        'Local',
+        'Express'
+      )
     ) +
     ggplot2::annotate(
       "text",
@@ -196,7 +248,7 @@ plot_productivity_distribution <- function(
       )
     ) +
     style_kcm(textsize = style_size) +
-    ggplot2::theme(legend.key = ggplot2::element_rect(fill = "#0072BC"))
+    ggplot2::theme(legend.key = ggplot2::element_rect(fill = "#EFEFEF"))
 
   plt
 }
